c02ea2f39c5a32138711e4a7ecab5f91af9299a4
alaca-baris <63210986+alaca-baris@users.noreply.github.com>
2020-07-30 10:38:20 +0200

    Create GWASrrBLUP.R

---
+++ GWASrrBLUP.R
@@ -0,0 +1,24 @@
+install.packages("rrBLUP")
+library(rrBLUP)
+
+
+outfileABHTwithpos2 <- t(outfileABHTwithpos)
+write.table(outfileABHTwithpos2, row.names = F, col.names = F, file = "GWASgeno.txt")
+GWASall <- GWAS(pheno = PhenoBLUP, geno = GWASgeno,plot = FALSE)
+write.table(GWASall,row.names = F, col.names = T, file = "GWASall.txt")
+
+#random population of 200 lines with 1000 markers
+M <- matrix(rep(0,200*1000),1000,200)
+for (i in 1:200) {
+  M[,i] <- ifelse(runif(1000)<0.5,-1,1)
+}
+colnames(M) <- 1:200
+genorandom <- data.frame(marker=1:1000,chrom=rep(1,1000),pos=1:1000,M,check.names=FALSE)
+
+phenotessstt <- data.frame(line=1:200,y=y)
+y <- g + rnorm(200,mean=0,sd=sqrt((1-h2)/h2*var(g)))
+g <- as.vector(crossprod(M,u))
+QTL <- 100*(1:5) #pick 5 QTL
+u <- rep(0,1000) #marker effects
+u[QTL] <- 1
+h2 <- 0.5
